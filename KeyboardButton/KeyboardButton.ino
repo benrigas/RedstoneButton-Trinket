285494d1271c44cdb6ef5c948b0a0baebeb7353f
Ben Rigas <ben.rigas@modmed.com>
2014-02-14 15:32:44 -0500

    latest button code with real shortcut

--- KeyboardButton/KeyboardButton.ino
+++ KeyboardButton/KeyboardButton.ino
@@ -1,7 +1,9 @@
+
 #include <TrinketKeyboard.h>
 
 const int buttonPin = 0;
 const int ledPin = 2;
+unsigned long lastButtonTime;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
@@ -14,8 +16,12 @@ void loop() {
   TrinketKeyboard.poll();
 
   if (! digitalRead(buttonPin)) {  // if the button is pressed
-    digitalWrite(ledPin, HIGH);    // light up the LED
-    TrinketKeyboard.print("A");
+    if (millis() - lastButtonTime > 5000) {
+      digitalWrite(ledPin, HIGH);    // light up the LED
+      TrinketKeyboard.pressKey(KEYCODE_MOD_LEFT_SHIFT|KEYCODE_MOD_LEFT_CONTROL|KEYCODE_MOD_LEFT_GUI, KEYCODE_B);
+      TrinketKeyboard.pressKey(0,0);
+      lastButtonTime = millis();
+    }
   } else {
     digitalWrite(ledPin, LOW);     // otherwise, turn it off
   }
